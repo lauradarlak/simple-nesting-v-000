7943796926b54a5d74d1390d8838405f45f73a02
Laura Darlak <laura@polyporedesign.com>
2018-08-21 00:39:23 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -117,11 +117,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-binding.pry
 		 programmer_hash
 
 end
-changing_alan
 
 def adding_to_dennis
 	programmer_hash =
@@ -140,6 +138,7 @@ def adding_to_dennis
         }
      }
 
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 		 return programmer_hash
 
 end
